7bf85985b66abb1f6967befeeb284ef67018661e
JefersonBurgos <jefersonburgos98@gmail.com>
2025-04-13 22:50:30 -0500

    Add DTO and service implement

---
+++ src/main/java/com/example/seguros_api_example/dto/ClienteDTO.java
@@ -0,0 +1,13 @@
+package com.example.seguros_api_example.dto;
+
+import lombok.Data;
+
+@Data
+public class ClienteDTO {
+
+    private String nombre;
+    private String email;
+    private String telefono;
+
+}
+
